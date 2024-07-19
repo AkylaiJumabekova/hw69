d7b86b9327e75ca4b797dd3378e8a572cb9ef335
AkylaiJumabekova <jumabekovaakylai7@gmail.com>
2024-07-19 19:01:38 +0600

    added styles to detailed info

--- src/containers/ShowDetails/ShowDetails.tsx
+++ src/containers/ShowDetails/ShowDetails.tsx
@@ -1,13 +1,12 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchShowDetails } from './ShowDetailsSlice';
 import { RootState, AppDispatch } from '../../app/store';
 
 const ShowDetails: React.FC = () => {
     const { id } = useParams<{ id: string }>();
     const dispatch: AppDispatch = useDispatch();
-
     const show = useSelector((state: RootState) => state.showDetails.show);
     const isLoading = useSelector((state: RootState) => state.showDetails.isLoading);
     const isError = useSelector((state: RootState) => state.showDetails.isError);
@@ -19,18 +18,30 @@ const ShowDetails: React.FC = () => {
     }, [dispatch, id]);
 
     if (isLoading) {
-        return <div>Loading...</div>;
+        return <p>Loading...</p>;
     }
 
-    if (isError || !show) {
-        return <div>Error loading show details</div>;
+    if (isError) {
+        return <p>Error loading show details</p>;
     }
+
+    if (!show) {
+        return <p>No show details available</p>;
+    }
+
+    const cleanSummary = show.summary ? show.summary.replace(/<\/?[^>]+(>|$)/g, "") : "";
 
     return (
         <div className="container mt-4">
-            <h1>{show.name}</h1>
-            <p>{show.summary}</p>
-            <img src={show.image?.medium} alt={show.name} />
+            <div className="row">
+                <div className="col-md-4">
+                    {show.image && <img src={show.image.medium} alt={show.name} className="img-fluid" />}
+                </div>
+                <div className="col-md-8">
+                    <h1>{show.name}</h1>
+                    <p className="mt-3">{cleanSummary}</p>
+                </div>
+            </div>
         </div>
     );
 };
